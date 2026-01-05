00ae88c340e8857a734f722c224b6bba903b19e5
Princewill Isonguyo <princewillisonguyo@gmail.com>
2026-01-06 00:57:24 +0100

    Update navbar height and logo image source

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,10 +25,10 @@ export function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-background/95 backdrop-blur-sm border-b border-border">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-20">
+        <div className="flex items-center justify-between h-30">
           {/* Logo */}
           <Link href="/" className="flex items-center">
-         <img src="/cloth-logo(2).jfif"alt="AFROLUXE"className="h-30 max-w-[1000px] w-auto object-contain"/>
+         <img src="/cloth-logo(1).jfif"alt="AFROLUXE"className="h-30 max-w-[5000px] w-auto object-contain"/>
           </Link>
 
           {/* Desktop Navigation */}
