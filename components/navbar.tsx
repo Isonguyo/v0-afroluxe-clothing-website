161d1647f006e76aa6c5aadcbd183f353a985264
Princewill Isonguyo <princewillisonguyo@gmail.com>
2026-01-05 23:19:55 +0100

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,7 +28,7 @@ export function Navbar() {
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <Link href="/" className="flex items-center">
-          <img src="/images/Cloth-logo.jfif"alt="AFROLUXE"className="h-8 w-auto"/>
+          <img src="/public/Cloth-logo.jfif"alt="AFROLUXE"className="h-8 w-auto"/>
           </Link>
 
           {/* Desktop Navigation */}
